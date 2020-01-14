ad08271ba1f3118c456e3f192f5eac1088c2a4be
MadLars <59825479+MadLars@users.noreply.github.com>
2020-01-14 09:27:30 +0100

    Update for readability

--- Day_1_part_1/aocday1.py
+++ Day_1_part_1/aocday1.py
@@ -2,6 +2,19 @@ import math
 import csv
 
 sumall = 0
+
+with open("C:\\Users\\gollu\\Desktop\\aoc_day1_input.csv", "r") as f:
+     reader = csv.reader(f)
+     for row in reader:       
+          y=''.join(map(str, row))
+          z=int(y)          
+          #print("this is z  ", z)
+          sum = math.floor((z) / 3)-2
+          sumall += sum
+          #print("this is sumall inner  ", sumall)
+
+print(sumall)
+
 # while True:
 #     mass = input("Enter the mass of the module: ")
 #     sum = math.floor(int(mass) / 3)-2
@@ -13,16 +26,3 @@ sumall = 0
 #          break
 #     elif YesorNo == "n":
 #          break
-
-with open("C:\\Users\\gollu\\Desktop\\aoc_day1_input.csv", "r") as f:
-     reader = csv.reader(f)
-     for row in reader:
-          
-          y=''.join(map(str, row))
-          z=int(y)          
-          #print("this is z  ", z)
-          sum = math.floor((z) / 3)-2
-          sumall += sum
-          #print("this is sumall inner  ", sumall)
-
-print(sumall)
